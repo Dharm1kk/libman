25474ea54148521b44d7cc53b4dceddb530e32aa
dharm1kk <dharmikshah1634@gmail.com>
2026-03-17 17:23:33 +0530

    added find by id and delete

--- app/controller/controller.js
+++ app/controller/controller.js
@@ -1,10 +1,10 @@
 const Book = require('../model/model.js');
 
-exports.create = (req,res)=>{
+exports.create = (req, res) => {
 
-    if(!req.body.title || !req.body.bookId){
+    if (!req.body.title || !req.body.bookId) {
         return res.status(400).send({
-            message:"Book title and ID cannot be empty"
+            message: "Book title and ID cannot be empty"
         });
     }
 
@@ -15,12 +15,61 @@ exports.create = (req,res)=>{
     });
 
     book.save()
-    .then(data=>{
-        res.send(data);
-    })
-    .catch(err=>{
-        res.status(500).send({
-            message: err.message || "Some error occurred while creating the Book."
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message || "Some error occurred while creating the Book."
+            });
         });
-    });
-}
+};
+
+exports.findAll = (req, res) => {
+    Book.find()
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message
+            });
+        });
+};
+
+exports.findOne = (req, res) => {
+    Book.findOne({ bookId: Number(req.params.bookId) }) // ✅ fix
+        .then(book => {
+            if (!book) {
+                return res.status(404).send({ // ✅ fix
+                    message: "Book not found with that ID"
+                });
+            }
+            res.send(book);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: "Cannot retrieve book with that ID"
+            });
+        });
+};
+
+exports.delete = (req, res) => {
+    Book.findOneAndDelete({ bookId: Number(req.params.bookId) })
+        .then(book => {
+            if (!book) {
+                return res.status(404).send({
+                    message: "Book not found with that ID"
+                });
+            }
+
+            res.send({ // ✅ IMPORTANT FIX
+                message: "Book deleted successfully"
+            });
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: "Error deleting book"
+            });
+        });
+};
--- app/routes/routes.js
+++ app/routes/routes.js
@@ -2,5 +2,7 @@ module.exports = (app)=>{
     const book = require('../controller/controller.js');
 
     app.post('/book',book.create);
-
+    app.get('/book/find',book.findAll);
+    app.get('/book/find/:bookId',book.findOne);   
+    app.delete('/book/delete/:bookId',book.delete);
 }
